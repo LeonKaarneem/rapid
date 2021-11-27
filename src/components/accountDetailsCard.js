01681f4731d9961a46527b78e16060529eaa4afd
kuldarvakker <kuvakk@taltech.ee>
2021-11-27 03:34:45 +0200

    Dashboard -> accountDetailsCard component

---
+++ src/components/accountDetailsCard.js
@@ -0,0 +1,31 @@
+import './accountDetailsCard.css';
+import Card from "./card";
+
+const AccountDetailsCard = (props) => {
+    return(
+        <Card leftHeader={"Balance"} rightHeader={"Available funds"} content={Content(props)}/>
+    );
+}
+
+const Content = (props) => {
+
+    let availableFundsArray = JSON.parse(props.availableFunds);
+
+    return(
+        <div className={"content"}>
+            <div className={"contentLeftColumn"}>
+                <span className={"accountName"}>{props.accountName}</span>
+                <span className={"accountNumber"}>{props.accountNumber}</span>
+            </div>
+            <div className={"contentRightColumn"}>
+                {availableFundsArray.map((value) => {
+                    return <span>{value}</span>
+                })}
+            </div>
+        </div>
+    );
+}
+
+
+
+export default AccountDetailsCard;
