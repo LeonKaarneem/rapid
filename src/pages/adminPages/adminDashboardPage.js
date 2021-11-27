234092f7669a78b11a934cba05caf84ac0397037
LeonKaarneem <LeonKaarneem@gmail.com>
2021-11-27 07:52:47 +0200

    dashboard layout

--- src/pages/adminPages/adminDashboardPage.js
+++ src/pages/adminPages/adminDashboardPage.js
@@ -1,9 +1,17 @@
 import Layout from '../../components/layout.js';
+import StatisticsMonthCard from "../../components/statisticsMonthCard";
 
 const AdminDashboardPage = () => {
     return (
         <Layout>
-            admin page content
+            <div style={{display: 'flex', flexWrap: 'wrap'}}>
+                <div style={{flex: '1 1 50%'}}>
+                    <StatisticsMonthCard title={"Transactions"} todayValue={"21 403"} sevenDayValue={"149 821"} thirtyDayValue={"1 449 463"} />
+                </div>
+                <div style={{flex: '1 1 50%'}}>
+                    <StatisticsMonthCard title={"Transactions"} todayValue={"21 403"} sevenDayValue={"149 821"} thirtyDayValue={"1 449 463"} />
+                </div>
+            </div>
         </Layout>
     );
 }
