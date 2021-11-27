17b742b5c30e5e9bb93d49ccddc1775205d3277e
kuldarvakker <kuvakk@taltech.ee>
2021-11-27 03:25:55 +0200

    Card component

---
+++ src/components/card.js
@@ -0,0 +1,23 @@
+
+import './card.css';
+
+const Card = (props) => {
+    return (
+        <div className={"card"}>
+            <div>
+                <div className={"cardHeader"}>
+                    <div>{props.leftHeader}</div>
+                    <div>{props.rightHeader}</div>
+                </div>
+                <div className={"cardContent"}>
+                    <div>{props.content}</div>
+                </div>
+            </div>
+            <div className={"cardFooter"}>
+                <div>{props.footer}</div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
